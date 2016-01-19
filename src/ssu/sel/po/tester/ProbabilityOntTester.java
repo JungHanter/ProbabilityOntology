7fd7dfe14c54f4adca51c74bb9e99a9d568172de
Hanter Jung <hanterkr@gmail.com>
2016-01-19 18:20:17 +0900

    test

--- src/ssu/sel/po/tester/ProbabilityOntTester.java
+++ src/ssu/sel/po/tester/ProbabilityOntTester.java
@@ -25,9 +25,9 @@ public class ProbabilityOntTester {
         analyzer.addAlignmentSchemaOntology(HEALTH_CONTEXT_ONTOLOGY, HCTX_ONT_NAMESPACE, "hctx", "RDF/XML");
         analyzer.addAlignmentDataOntology(HEALTH_CONTEXT_ONTOLOGY, HCTX_ONT_NAMESPACE, "hctx", "RDF/XML");
 
-        analyzer.addAlignmentDataOntology(makeTestLearningDataset(analyzer.getAlignedSchemaOntModel(), 100),
+        analyzer.addAlignmentDataOntology(makeTestLearningDataset(analyzer.getAlignedSchemaOntModel(), 1000),
                 HCTX_ONT_NAMESPACE, "hctx");
-        analyzer.addAlignmentDataOntology(makeTestAnalysintInputDataset(analyzer.getAlignedSchemaOntModel(), 3),
+        analyzer.addAlignmentDataOntology(makeTestAnalysintInputDataset(analyzer.getAlignedSchemaOntModel(), 50),
                 HCTX_ONT_NAMESPACE, "hctx");
 
         Model resultModel = analyzer.analyze(HCTX_ONT_NAMESPACE);
